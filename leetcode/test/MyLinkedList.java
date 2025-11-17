3966e2b6d3cdad8f9b486915f3c4415474986be4
LingMo <2101454328a@gmail.com>
2025-11-17 14:01:57 +0800

    新增实现 707. 设计链表 的剩余部分

--- leetcode/test/MyLinkedList.java
+++ leetcode/test/MyLinkedList.java
@@ -1,5 +1,6 @@
 package src.leetcode.test;
 
+//707. 设计链表
 public class MyLinkedList {
     int val;
     MyLinkedList next;
@@ -32,6 +33,7 @@ public class MyLinkedList {
         return n;
     }
 
+    //打印链表数据
     public  void  showList(){
         System.out.print("[");
         MyLinkedList linkedList=this.head;
@@ -46,24 +48,71 @@ public class MyLinkedList {
     }
 
 
+    //获取链表中下标为 index 的节点的值。如果下标无效，则返回 -1
     public int get(int index) {
+        MyLinkedList list=this.head;
+        if (index<0)
+            return -1;
+        while (list!=null){
+            if (index==0)
+                return list.val;
+            list=list.next;
+            index--;
+        }
         return -1;
     }
 
+    // 将一个值为 val 的节点插入到链表中第一个元素之前。在插入完成后，新节点会成为链表的第一个节点。
     public void addAtHead(int val) {
-
+        this.head=new MyLinkedList(val,head);
     }
 
+    //将一个值为 val 的节点追加到链表中作为链表的最后一个元素。
     public void addAtTail(int val) {
-
+        if (head==null){
+            head=new MyLinkedList(val,null);
+            return;
+        }
+        MyLinkedList cur= this.head;
+        while (cur.next!=null){
+            cur=cur.next;
+        }
+        cur.next=new MyLinkedList(val,null);
     }
 
+    // 将一个值为 val 的节点插入到链表中下标为 index 的节点之前。
+    // 如果 index 等于链表的长度，那么该节点会被追加到链表的末尾。
+    // 如果 index 比长度更大，该节点将 不会插入 到链表中。
     public void addAtIndex(int index, int val) {
-
+        MyLinkedList dummy=new MyLinkedList(0,this.head);
+        if (index==0){
+            head=new MyLinkedList(val,head);
+        }
+        while (dummy.next!=null&&index!=0){
+            dummy=dummy.next;
+            index--;
+        }
+        if (index==0){
+            MyLinkedList temp = dummy.next;
+            dummy.next=new MyLinkedList(val,temp);
+        }
     }
 
+    //如果下标有效，则删除链表中下标为 index 的节点。
     public void deleteAtIndex(int index) {
-
+        if (index==0&&head!=null){
+            head=head.next;
+        }
+        MyLinkedList dummy=new MyLinkedList(0,this.head);
+        while (dummy.next!=null&&index!=0){
+            dummy=dummy.next;
+            index--;
+        }
+        if (index==0){
+            if (dummy.next != null) {
+                dummy.next=dummy.next.next;
+            }
+        }
     }
 }
 
--- leetcode/test/MyLinkedListTest.java
+++ leetcode/test/MyLinkedListTest.java
@@ -3,9 +3,25 @@ package src.leetcode.test;
 public class MyLinkedListTest {
     public static void main(String[] args) {
         int[] a={1,2,3};
-        MyLinkedList list=new MyLinkedList(newList(a));
-        list.showList();
-        System.out.println(list.length());
+        //MyLinkedList list=new MyLinkedList(newList(a));
+        MyLinkedList list=new MyLinkedList();
+        //list.showList();
+//        System.out.println(list.length());
+//        System.out.println(list.get(2));
+//        list.addAtHead(1);
+//        list.addAtTail(99);
+//        list.addAtIndex(5,66);
+//        list.deleteAtIndex(6);
+//        list.showList();
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        //["MyLinkedList","addAtTail","addAtTail","get"] [[],[1],[3],[1]]
+        myLinkedList.showList();
+        myLinkedList.addAtTail(1);
+        myLinkedList.showList();
+        myLinkedList.addAtTail(3);
+        myLinkedList.showList();
+
     }
 
 
