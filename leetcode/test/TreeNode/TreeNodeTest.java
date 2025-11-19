6fd640a386ea44205d360fcd98119d342fb8e595
LingMo <2101454328a@gmail.com>
2025-11-19 21:23:23 +0800

    新增 144. 二叉树的前序遍历

--- leetcode/test/TreeNode/TreeNodeTest.java
+++ leetcode/test/TreeNode/TreeNodeTest.java
@@ -1,5 +1,6 @@
 package src.leetcode.test.TreeNode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TreeNodeTest {
@@ -11,4 +12,17 @@ public class TreeNodeTest {
 
 class Solution {
 
+    //144. 二叉树的前序遍历
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        pt(root,list);
+        return list;
+    }
+    public void pt(TreeNode cur,List<Integer> list){
+        if (cur==null)
+            return;
+        list.add(cur.val);
+        pt(cur.left,list);
+        pt(cur.right,list);
+    }
 }
