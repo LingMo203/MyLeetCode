db99955d264fa7cf26b3192d2541078d4f6d532a
LingMo <2101454328a@gmail.com>
2025-11-19 21:38:01 +0800

    新增 94. 二叉树的中序遍历 145. 二叉树的后序遍历

--- leetcode/test/TreeNode/TreeNodeTest.java
+++ leetcode/test/TreeNode/TreeNodeTest.java
@@ -15,14 +15,52 @@ class Solution {
     //144. 二叉树的前序遍历
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> list=new ArrayList<>();
-        pt(root,list);
+        preorderTraversalImplement(root,list);
         return list;
     }
-    public void pt(TreeNode cur,List<Integer> list){
+    public void preorderTraversalImplement(TreeNode cur,List<Integer> list){
         if (cur==null)
             return;
         list.add(cur.val);
-        pt(cur.left,list);
-        pt(cur.right,list);
+        preorderTraversalImplement(cur.left,list);
+        preorderTraversalImplement(cur.right,list);
     }
+
+    //94. 二叉树的中序遍历
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        inorderTraversalImplement(root,list);
+        return list;
+    }
+    public void inorderTraversalImplement(TreeNode cur,List<Integer> list){
+        if (cur==null)
+            return;
+        inorderTraversalImplement(cur.left,list);
+        list.add(cur.val);
+        inorderTraversalImplement(cur.right,list);
+    }
+
+    //145. 二叉树的后序遍历
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        postorderTraversalImplement(root,list);
+        return list;
+    }
+    public void postorderTraversalImplement(TreeNode cur,List<Integer> list){
+        if (cur==null)
+            return;
+        postorderTraversalImplement(cur.left,list);
+        postorderTraversalImplement(cur.right,list);
+        list.add(cur.val);
+    }
+
+
+
+
+
+
+
+
+
+
 }
