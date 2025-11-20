9ed278daa2bddced4cd0080f668fc61dee21033a
LingMo <2101454328a@gmail.com>
2025-11-20 15:50:57 +0800

    新增 226. 翻转二叉树

--- leetcode/test/TreeNode/TreeNodeTest.java
+++ leetcode/test/TreeNode/TreeNodeTest.java
@@ -136,6 +136,17 @@ class Solution {
         return result;
     }
 
+    //226. 翻转二叉树
+    public TreeNode invertTree(TreeNode root) {
+        if (root==null) return root;
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
 
 
 
