910642dd6030ec22e1e7cf5dffc0b66bdba3f614
LingMo <2101454328a@gmail.com>
2025-11-22 21:42:57 +0800

    新增 509. 斐波那契数

---
+++ leetcode/test/DynamicProgramming.java
@@ -0,0 +1,33 @@
+package src.leetcode.test;
+
+public class DynamicProgramming {
+    public static void main(String[] args) {
+        DynamicProgramming dp=new DynamicProgramming();
+        System.out.println(dp.fib(1));
+    }
+
+
+    //509. 斐波那契数
+    public int fib(int n) {
+        if (n==0) return 0;
+        int[] fb=new int[2];
+        fb[0]=0;
+        fb[1]=1;
+        for (int i=2;i<=n;i++){
+            int sum=fb[0]+fb[1];
+            fb[0]=fb[1];
+            fb[1]=sum;
+        }
+        return fb[1];
+    }
+
+
+
+
+
+
+
+
+
+
+}
