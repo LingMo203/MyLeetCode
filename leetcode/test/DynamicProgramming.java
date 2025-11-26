87f7f4b29db83ded75b52c9441ae8efddc20d1aa
LingMo <2101454328a@gmail.com>
2025-11-26 21:34:10 +0800

    新增 119. 杨辉三角II

--- leetcode/test/DynamicProgramming.java
+++ leetcode/test/DynamicProgramming.java
@@ -13,7 +13,8 @@ public class DynamicProgramming {
         //System.out.println(dp.minCostClimbingStairs(nums));
         //System.out.println(dp.uniquePaths(5,5));
         //System.out.println(dp.uniquePathsWithObstacles(dnums));
-        System.out.println(dp.generate(5));
+        //System.out.println(dp.generate(5));
+        System.out.println(dp.getRow(5));
 
     }
 
@@ -118,6 +119,24 @@ public class DynamicProgramming {
             result.add(list);
         }
         return result;
+    }
+
+    //119. 杨辉三角II
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list=new ArrayList<>();
+        list.add(1);
+        if (rowIndex==0) return list;
+        list.add(1);
+        for (int i=1;i<=rowIndex-1;i++){
+            List<Integer> temp=new ArrayList<>();
+            temp.add(1);
+            for (int j=1;j<=i;j++){
+                temp.add(list.get(j-1)+list.get(j));
+            }
+            temp.add(1);
+            list=temp;
+        }
+        return list;
     }
 
 
