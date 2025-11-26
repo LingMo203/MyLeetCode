0db0242b16dea27ba59d4251b0f426fa2d3a25d0
LingMo <2101454328a@gmail.com>
2025-11-26 15:49:09 +0800

    新增 63. 不同路径II

--- leetcode/test/DynamicProgramming.java
+++ leetcode/test/DynamicProgramming.java
@@ -1,12 +1,19 @@
 package src.leetcode.test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class DynamicProgramming {
     public static void main(String[] args) {
         DynamicProgramming dp=new DynamicProgramming();
         int[] nums={0,2,2,1};
+        //int[][] dnums={{0,0,0,0},{0,1,0,0},{0,0,0,0},{0,0,1,0},{0,0,0,0}};
+        int[][] dnums={{0,0},{1,1},{0,0}};
         //System.out.println(dp.fib(1));
         //System.out.println(dp.minCostClimbingStairs(nums));
-        System.out.println(dp.uniquePaths(5,5));
+        //System.out.println(dp.uniquePaths(5,5));
+        System.out.println(dp.uniquePathsWithObstacles(dnums));
+
     }
 
 
@@ -57,6 +64,35 @@ public class DynamicProgramming {
         for (int i=2;i<=m;i++){
             for (int j=2;j<=n;j++){
                 dp[i][j]=dp[i][j-1]+dp[i-1][j];
+            }
+        }
+        return dp[m][n];
+    }
+
+    //63. 不同路径II 太艰难了 12次提交
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length-1,n=obstacleGrid[0].length-1;
+        if (obstacleGrid[m][n]==1||obstacleGrid[0][0]==1) return 0;
+        int[][] dp=new int[m+1][n+1];
+        for (int i=0;i<=m;i++){
+            if (obstacleGrid[i][0]==1) {
+                dp[i][0] = 0;
+                break;
+            }
+            else dp[i][0]=1;
+        }
+        for (int i=0;i<=n;i++){
+            if (obstacleGrid[0][i]==1) {
+                dp[0][i] = 0;
+                break;
+            }
+            else dp[0][i]=1;
+        }
+
+        for (int i=1;i<=m;i++){
+            for (int j=1;j<=n;j++){
+                if (obstacleGrid[i][j]==1) dp[i][j]=0;
+                else dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
         }
         return dp[m][n];
