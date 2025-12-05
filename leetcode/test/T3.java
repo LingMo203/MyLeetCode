f7c4aed3fe26b953cf5f9b7f738fd51b94d44bca
LingMo <2101454328a@gmail.com>
2025-12-05 22:57:18 +0800

    新增 136. 只出现一次的数字

--- leetcode/test/T3.java
+++ leetcode/test/T3.java
@@ -456,6 +456,22 @@ class Solution3 {
         System.arraycopy(be,0,nums,end.length,be.length);
     }
 
+    //136. 只出现一次的数字
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> hashSet=new HashSet<>();
+        for (int num:nums){
+            if (!hashSet.contains(num)){
+                hashSet.add(num);
+            }else {
+                hashSet.remove(num);
+            }
+        }
+        for (int num:hashSet){
+            return num;
+        }
+        return -1;
+    }
+
 
 
 
