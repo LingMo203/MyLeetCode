c63214e48a6df65845515df80dcfc352fba71058
LingMo <2101454328a@gmail.com>
2025-12-11 17:25:56 +0800

    新增 137. 只出现一次的数字 II

--- leetcode/test/T4.java
+++ leetcode/test/T4.java
@@ -415,6 +415,23 @@ public class T4 {
         return result;
     }
 
+    //137. 只出现一次的数字 II
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> hashMap=new HashMap<>();
+        for (int num:nums){
+            if (!hashMap.containsKey(num)){
+                hashMap.put(num,1);
+            }else {
+                hashMap.put(num,3);
+            }
+        }
+        for (Map.Entry<Integer,Integer> map:hashMap.entrySet()){
+            int val=map.getValue();
+            if (val==1) return map.getKey();
+        }
+        return 0;
+    }
+
 
 
 
