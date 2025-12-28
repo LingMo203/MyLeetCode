84bbd356a813f9e3707ab11984becc8c7f3e874b
LingMo <2101454328a@gmail.com>
2025-12-28 22:13:02 +0800

    新增 64. 最小路径和

--- src/test/DynamicProgramming.java
+++ src/test/DynamicProgramming.java
@@ -8,7 +8,7 @@ public class DynamicProgramming {
         DynamicProgramming dp=new DynamicProgramming();
         int[] nums={2,3,1,1,4};
         //int[][] dnums={{0,0,0,0},{0,1,0,0},{0,0,0,0},{0,0,1,0},{0,0,0,0}};
-        int[][] dnums={{0,0},{1,1},{0,0}};
+        int[][] dnums={{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
         String strs="aaabaaaabaaabaaaabaaaabaaaabaaaaba";
         String strs2="aaaba";
         //System.out.println(dp.fib(1));
@@ -21,7 +21,8 @@ public class DynamicProgramming {
         //System.out.println(dp.maxRepeating(strs,strs2));
         //System.out.println(dp.canJump(nums));
         //System.out.println(dp.maxSubArray(nums));
-        System.out.println(dp.jump(nums));
+        //System.out.println(dp.jump(nums));
+        System.out.println(dp.minPathSum(dnums));
     }
 
 
@@ -238,7 +239,24 @@ public class DynamicProgramming {
         return count;
     }
 
-
+    //64. 最小路径和
+    public int minPathSum(int[][] grid) {
+        int m=grid[0].length,n=grid.length;
+        int[][] dp=new int[n][m];
+        dp[0][0]=grid[0][0];
+        for (int i=1;i<m;i++){
+            dp[0][i]=grid[0][i]+dp[0][i-1];
+        }
+        for (int i=1;i<n;i++){
+            dp[i][0]=grid[i][0]+dp[i-1][0];
+        }
+        for (int i=1;i<n;i++){
+            for (int j=1;j<m;j++){
+                dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n-1][m-1];
+    }
 
 
 
