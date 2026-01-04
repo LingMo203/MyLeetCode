effbb8c3cec5bb5d56fc88cfd15190ade3ed7a84
LingMo <2101454328a@gmail.com>
2026-01-04 23:25:12 +0800

    新增 39. 组合总和

--- src/test/BackTrack.java
+++ src/test/BackTrack.java
@@ -7,7 +7,9 @@ import java.util.List;
 public class BackTrack {
     public static void main(String[] args) {
         BackTrack bt=new BackTrack();
-        System.out.println(bt.combine(4,2));
+        int[] nums={2,3,6,7};
+        //System.out.println(bt.combine(4,2));
+        System.out.println(bt.combinationSum(nums,7));
     }
 
 
@@ -30,6 +32,30 @@ public class BackTrack {
             path.remove(path.size()-1);
         }
         return;
+    }
+
+    //39. 组合总和
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> path=new ArrayList<>();
+        backTrackingCombinationSum(res,path,0,candidates,target,0);
+        return res;
+    }
+    public void backTrackingCombinationSum(List<List<Integer>> res,List<Integer> path,int index,int[] candidates, int target,int sum){
+        if (sum>=target){
+            if (sum==target){
+                res.add(new ArrayList<>(path));
+            }
+            return;
+        }
+        for (int i=index; i<candidates.length;i++) {
+            int num=candidates[i];
+            path.add(num);
+            sum+=num;
+            backTrackingCombinationSum(res, path,i, candidates, target, sum);
+            int t =  path.remove(path.size()-1);
+            sum-=t;
+        }
     }
 
 
