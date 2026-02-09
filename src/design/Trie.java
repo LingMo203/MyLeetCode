f4a3f448899125cf40ba3fd07417af398d19f53e
LingMo <2101454328a@gmail.com>
2026-02-10 02:08:09 +0800

    更新 208. 实现 Trie (前缀树) 真正实现前缀树(未优化)

--- src/design/Trie.java
+++ src/design/Trie.java
@@ -4,30 +4,54 @@ import java.util.HashSet;
 
 //208. 实现 Trie (前缀树)
 class Trie {
-
-    HashSet<String> wordSet;
-    HashSet<String> prefixSet;
+    Node root;
 
     public Trie() {
-        wordSet=new HashSet<>();
-        prefixSet=new HashSet<>();
+        root = new Node();
     }
 
     public void insert(String word) {
-        wordSet.add(word);
-        StringBuilder sb=new StringBuilder(word);
-        while (!sb.isEmpty()){
-            prefixSet.add(sb.toString());
-            sb.deleteCharAt(sb.length()-1);
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            int id = word.charAt(i) - 'a';
+            if (!cur.vis[id]){
+                cur.vis[id] = true;
+                cur.next[id] = new Node();
+            }
+            cur = cur.next[id];
         }
+        cur.isEnd = true;
     }
 
     public boolean search(String word) {
-        return wordSet.contains(word);
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            int id = word.charAt(i) - 'a';
+            if (!cur.vis[id]) return false;
+            cur = cur.next[id];
+        }
+        return cur.isEnd;
     }
 
     public boolean startsWith(String prefix) {
-        return prefixSet.contains(prefix);
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            int id = prefix.charAt(i) - 'a';
+            if (!cur.vis[id]) return false;
+            cur = cur.next[id];
+        }
+        return true;
+    }
+}
+
+class Node {
+    boolean[] vis;
+    Node[] next;
+    boolean isEnd;
+
+    public Node() {
+        vis = new boolean[26];
+        next = new Node[26];
     }
 }
 
