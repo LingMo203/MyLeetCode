1442b2b1bbb0f9e8ccdee98e5323d6bca3c6190a
LingMo <2101454328a@gmail.com>
2026-02-18 20:48:50 +0800

    新增 50. Pow(x, n) (快速幂)

--- src/test/T6.java
+++ src/test/T6.java
@@ -460,6 +460,17 @@ public class T6 {
         return count;
     }
 
+    //50. Pow(x, n) (快速幂)
+    public double myPow(double x, int n) {
+        return n >= 0 ? quickPow(x, n) : 1 / quickPow(x, -n);
+    }
+    public double quickPow(double x, int n) {
+        if (n == 0) return 1;
+        else if (n == 1) return x;
+        double num = quickPow(x, n / 2);
+        return n % 2 == 0 ? num * num : num * num * x;
+    }
+
 }
 
 
